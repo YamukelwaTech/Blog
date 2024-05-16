a7e0da285f562118cb125b6d09859a157f55b346
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-16 10:36:35 +0200

    Chore:deep clean

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello World</h1>
   </React.StrictMode>
 );
