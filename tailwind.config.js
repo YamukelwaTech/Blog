d45e47973a26d8865e916d45268343c954ffd6b2
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-21 20:05:28 +0200

    Chore:Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       colors: {
         customColor1: '#FDF8F0',
         customColor2: '#183a1d',
+        customColor3: '#FFAB00',
+        customColor4: '#DD2E18',
+        customColor5: '#582C12',
       },
       height: {
         '128': '52rem',
