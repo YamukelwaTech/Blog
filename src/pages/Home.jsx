f7980e9afd3e19d74c31b2bea93d895c3af2b8d7
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-21 23:27:32 +0200

    Chore:Hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,8 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Post from "../components/post";
-import Dashboard from "../components/detailedpost";
+import React from "react";
+import Hero from "../components/hero";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      detailedPost: null,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchPosts();
-  }
-
-  fetchPosts = () => {
-    axios
-      .get(`http://localhost:5000/posts`)
-      .then((response) => {
-        this.setState({ posts: response.data });
-      })
-      .catch((error) => {
-        console.error("Error fetching posts:", error);
-      });
-  };
-
-  fetchDetailedPost = (postToken) => {
-    axios
-      .get(`http://localhost:5000/posts/${postToken}`)
-      .then((response) => {
-        this.setState({ detailedPost: response.data });
-      })
-      .catch((error) => {
-        console.error("Error fetching detailed post information:", error);
-      });
-  };
-
-  handleShowDetailedPost = (postToken) => {
-    this.fetchDetailedPost(postToken);
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="flex flex-col md:flex-row mt-16">
-          <div className="flex-grow px-2 md:px-0 md:w-1/2">
-            {this.state.posts.map((post) => (
-              <div key={post.token} className="mb-4">
-                <div className="w-full">
-                  <Post
-                    post={post}
-                    onShowDetailedPost={this.handleShowDetailedPost}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-
-          <div className="hidden md:block md:w-1/2 px-4 fixed top-0 right-0">
-            <Dashboard detailedPost={this.state.detailedPost} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return <Hero />;
+};
 
 export default Home;
