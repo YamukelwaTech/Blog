1a0fc30e8b847aa8918884b6d219640b64bfb0c7
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-24 02:38:16 +0200

    Chore:Form Done

--- src/components/newpost.jsx
+++ src/components/newpost.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
 
 const Newpost = () => {
   const [formData, setFormData] = useState({
@@ -15,7 +15,7 @@ const Newpost = () => {
     backgroundimg: null,
   });
 
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate();
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
   const handleChange = (e) => {
@@ -59,41 +59,41 @@ const Newpost = () => {
       console.error("Upload failed:", error.message);
     }
   };
-  return (
-    <div className="w-full p-12 bg-customColor1">
-      <form onSubmit={handleSubmit}>
-        <div className="mt-10">
-          <div className="mb-4 md:mb-0 w-full mx-auto relative">
-            <div className="px-4 lg:px-0">
-              <h2 className="text-4xl font-semibold text-gray-800 leading-tight">
-                <input
-                  type="text"
-                  name="title"
-                  value={formData.title}
-                  onChange={handleChange}
-                  className="border-b-2 border-gray-800 focus:outline-none"
-                  placeholder="Enter main title"
-                />
-              </h2>
-              <p
-                href="#"
-                className="py-2 text-green-700 inline-flex items-center justify-center mb-2"
-              >
-                <input
-                  type="text"
-                  name="description"
-                  value={formData.description}
-                  onChange={handleChange}
-                  className="border-b-2 border-gray-800 focus:outline-none"
-                  placeholder="Enter article title"
-                />
-              </p>
-            </div>
 
-            <label
-              htmlFor="imageURL"
-              className="block mb-2 font-semibold text-gray-700"
-            >
+  return (
+    <div className="w-full p-4 md:p-8 lg:p-12 bg-customColor1 mt-8">
+      <form onSubmit={handleSubmit}>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          <div>
+            <label htmlFor="title" className="block mb-2 font-semibold text-gray-700">
+              Main Title:
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              className="border-b-2 border-gray-800 focus:outline-none bg-inherit w-full py-2"
+              placeholder="Enter main title"
+            />
+          </div>
+          <div>
+            <label htmlFor="description" className="block mb-2 font-semibold text-gray-700">
+              Article Title:
+            </label>
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              className="border-b-2 border-gray-800 focus:outline-none bg-inherit w-full py-2"
+              placeholder="Enter article title"
+            />
+          </div>
+          <div className="col-span-2">
+            <label htmlFor="imageURL" className="block mb-2 font-semibold text-gray-700">
               Upload Main Image:
             </label>
             <input
@@ -103,11 +103,9 @@ const Newpost = () => {
               onChange={(e) => handleImageChange(e, "imageURL")}
               className="py-2 text-green-700 inline-flex items-center justify-center mb-2"
             />
-
-            <label
-              htmlFor="backgroundimg"
-              className="block mb-2 font-semibold text-gray-700"
-            >
+          </div>
+          <div className="col-span-2">
+            <label htmlFor="backgroundimg" className="block mb-2 font-semibold text-gray-700">
               Upload Background Image:
             </label>
             <input
@@ -118,51 +116,47 @@ const Newpost = () => {
               className="py-2 text-green-700 inline-flex items-center justify-center mb-2"
             />
           </div>
-
-          <div className="flex flex-col lg:flex-row lg:space-x-12">
-            <div className="px-4 lg:px-0 mt-12 text-gray-700 text-lg leading-relaxed w-full lg:w-3/4">
-              <textarea
-                name="content"
-                value={formData.content}
-                onChange={handleChange}
-                className="pb-6 w-full"
-                placeholder="Enter text"
-                style={{ height: "30em" }}
-              ></textarea>
-            </div>
-
-            <div className="w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm">
-              <div className="p-4 border-t border-b md:border md:rounded">
-                <div className="flex py-2">
-                  <input
-                    type="text"
-                    name="author.name"
-                    value={formData["author.name"]}
-                    onChange={handleChange}
-                    placeholder="Name"
-                    className="font-semibold text-gray-700 text-sm w-full"
-                  />
-                  <input
-                    type="email"
-                    name="author.email"
-                    value={formData["author.email"]}
-                    onChange={handleChange}
-                    placeholder="Email"
-                    className="font-semibold text-gray-600 text-xs w-full mt-2"
-                  />
-                </div>
-
-                <button
-                  type="submit"
-                  className="px-2 py-1 text-customColor2 font-semibold bg-customColor3 flex w-full items-center justify-center rounded"
-                >
-                  Upload
-                  <i className="bx bx-user-plus ml-2"></i>
-                </button>
-              </div>
-            </div>
+          <div className="col-span-2">
+            <label htmlFor="content" className="block mb-2 font-semibold text-gray-700">
+              Content:
+            </label>
+            <textarea
+              name="content"
+              value={formData.content}
+              onChange={handleChange}
+              className="border-b-2 border-gray-800 focus:outline-none bg-inherit w-full py-2"
+              placeholder="Enter text"
+              style={{ height: "10em" }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="author.name"
+              value={formData["author.name"]}
+              onChange={handleChange}
+              placeholder="Name"
+              className="font-semibold text-gray-700 text-sm w-full border-b-2 border-gray-800 focus:outline-none bg-inherit py-2"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="author.email"
+              value={formData["author.email"]}
+              onChange={handleChange}
+              placeholder="Email"
+              className="font-semibold text-gray-600 text-xs w-full mt-2 border-b-2 border-gray-800 focus:outline-none bg-inherit py-2"
+            />
           </div>
         </div>
+        <button
+          type="submit"
+          className="mt-4 bg-customColor3 text-black px-4 py-2 rounded font-semibold"
+          disabled
+        >
+          Upload
+        </button>
       </form>
     </div>
   );
